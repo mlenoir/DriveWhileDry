c079e82183cee407fccebe9f99ff679a2e8aa7cd
Meagan Lenoir <melenoi@microsoft.com>
2016-07-26 13:06:16 -0700

    First Test Deployment

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ def home(request):
         'app/index.html',
         context_instance = RequestContext(request,
         {
-            'title':'Home Page',
+            'title':'Drive While Dry',
             'year':datetime.now().year,
         })
     )
